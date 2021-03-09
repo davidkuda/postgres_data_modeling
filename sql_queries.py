ef2a95a51655d93f3f4e24e3cdac2e8ed745308f
DavidKuda <david.kuda@pm.me>
2021-03-09 20:14:37 +0100

    fix: Reorder list according to FOREIGN KEYs

--- sql_queries.py
+++ sql_queries.py
@@ -101,6 +101,6 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create,
-                        time_table_create]
+create_table_queries = [artist_table_create, user_table_create, song_table_create,
+                        time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
