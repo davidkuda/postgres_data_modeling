0f412fe2da85621b0843cc5658c4942fbc8aec7d
DavidKuda <david.kuda@pm.me>
2021-03-15 23:12:44 +0100

    style: Break a line to reduce line length

--- src/sql_queries.py
+++ src/sql_queries.py
@@ -90,7 +90,8 @@ WHERE
 
 create_table_queries = [artist_table_create, user_table_create, song_table_create,
                         time_table_create, songplay_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop,
+                      artist_table_drop, time_table_drop]
 
 if __name__ == '__main__':
     def print_queries():
