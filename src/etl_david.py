0b818eca39a5649a049cb5bf77dc941a109b9667
DavidKuda <david.kuda@pm.me>
2021-03-16 20:15:37 +0100

    feat: Implement an alternative flow of the ETL

---
+++ src/etl_david.py
@@ -0,0 +1,129 @@
+import os
+import glob
+import sys
+from typing import List
+
+import psycopg2
+import pandas as pd
+
+from sql_queries import *
+from create_tables import drop_tables, create_tables
+
+
+class ETL:
+    def __init__(self, conn, cur):
+        self.conn = conn
+        self.cur = cur
+        # self.song_files_path = song_files_path
+        # self.log_files_path = log_files_path
+
+    @staticmethod
+    def get_all_json_files(filepath: str):
+        # get all files matching extension from directory
+        all_files = []
+        for root, dirs, files in os.walk(filepath):
+            files = glob.glob(os.path.join(root, '*.json'))
+            for f in files:
+                all_files.append(os.path.abspath(f))
+
+        # get total number of files found
+        num_files = len(all_files)
+        print(f'{num_files} files found in {filepath}')
+
+        return all_files
+
+    @staticmethod
+    def files_to_df(files: List[str]):
+        dfs = []
+        for file in files:
+            df = pd.read_json(file, lines=True)
+            dfs.append(df)
+        df = pd.concat(dfs, ignore_index=True)
+        return df
+
+
+def set_sys_path():
+    """Make sure that sys.path[0] is the directory "src"."""
+    current_path = os.path
+    print('Current Path:')
+    print(current_path)
+    correct_path = os.path.abspath('src/')
+    print(f'Setting path to {correct_path}')
+    # sys.path.insert(0, correct_path)
+
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+    conn.autocommit = True
+
+    etl = ETL(conn, cur)
+
+    # Get Song Data
+    file_path_song_data = '../data/song_data'
+    song_data_files = etl.get_all_json_files(file_path_song_data)
+    df_song_data = etl.files_to_df(song_data_files)
+    print(df_song_data.shape)
+
+    # Get Log Data
+    file_path_log_data = '../data/log_data'
+    log_data_files = etl.get_all_json_files(file_path_log_data)
+    df_log_data = etl.files_to_df(log_data_files)
+    print(df_log_data.shape)
+
+    # Filter by Pages that have "NextSong" as value
+    next_song_pages = df_log_data['page'] == 'NextSong'
+    df_log_data = df_log_data[next_song_pages]
+
+    # Upload Artists Data
+    artists_data = df_song_data[['artist_id', 'artist_name', 'artist_location',
+                                 'artist_latitude', 'artist_longitude']].values
+
+    for row in artists_data:
+        cur.execute(artist_table_insert, list(row))
+
+    # Upload Songs Data
+    songs_data = df_song_data[['song_id', 'title', 'artist_id', 'year', 'duration']].values
+    for row in songs_data:
+        cur.execute(song_table_insert, list(row))
+
+    # Extract Data for Time Table
+    timestamps = df_log_data['ts']
+    t = pd.to_datetime(timestamps)
+    time_data = [t, t.dt.hour, t.dt.day, t.dt.isocalendar().week, t.dt.month, t.dt.year, t.dt.weekday]
+    column_labels = ['timestamp', 'hour', 'day', 'week of year', 'month', 'year', 'weekday']
+    d = dict(zip(column_labels, time_data))
+    df_time = pd.DataFrame(d)
+
+    # Upload Time Data
+    for row in df_time.values:
+        cur.execute(time_table_insert, list(row))
+
+    # Upload Users Data
+    df_users = df_log_data[['userId', 'firstName', 'lastName', 'gender', 'level']]
+    for row in df_users.values:
+        cur.execute(user_table_insert, list(row))
+
+    # Upload Songplays Data
+    for index, row in df_log_data.iterrows():
+
+        # get song_id and artist_id from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+
+        if results:
+            song_id, artist_id = results
+        else:
+            song_id, artist_id = None, None
+
+        # insert songplay record
+        ids = pd.Series([song_id, artist_id], index=['song_id', 'artist_id'])
+        enriched_row = row.append(ids)
+        songplay_data = enriched_row[['ts', 'userId', 'level', 'song_id', 'artist_id',
+                                      'sessionId', 'location', 'userAgent']]
+        cur.execute(songplay_table_insert, list(songplay_data))
+
+
+if __name__ == '__main__':
+    set_sys_path()
+    main()
