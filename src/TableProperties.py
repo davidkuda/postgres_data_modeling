635f2010527b169fa1650d5b92b7afe8f3c0a740
DavidKuda <david.kuda@pm.me>
2021-03-16 20:48:01 +0100

    docs: Add docstrings to the methods

--- src/TableProperties.py
+++ src/TableProperties.py
@@ -3,7 +3,16 @@ from typing import List
 
 
 class TableProperties:
+    """Class that holds configuration / properties of the tables."""
     def __init__(self, table_name: str, table_properties: List[tuple]):
+        """Instantiate an object that holds properties / configuration of a table.
+
+        Args:
+            table_name (str): The name of the table, e.g. "songplays".
+            table_properties List[tuple]:
+              A list of tuples that contain two values. The first value is the column, the second value is the
+              configuration for that column. An example of one tuple might be: ('user_id', 'int primary key')
+        """
         self.table_name = table_name
         self.columns = [table_property[0] for table_property in table_properties]
         self.data_types = [table_property[1] for table_property in table_properties]
@@ -15,14 +24,20 @@ class TableProperties:
                         'select': f'SELECT {", ".join(self.columns)} FROM {self.table_name};'}
 
     def validate(self):
+        """Check if columns and configuration are equally long."""
         if len(self.columns) != len(self.data_types):
             raise ValueError('Make sure that columns and data_types are equally long.')
 
-    def concat_cols_with_types(self):
+    def concat_cols_with_types(self) -> List[str]:
+        """Returns a list with strings of statements for "create table" statements in SQL.
+
+        returns List[str]: A list of every create statement for the given table.
+        """
         create_statements = [f'{self.columns[index]} {self.data_types[index]}' for index in range(len(self.columns))]
         return create_statements
 
-    def get_query_create_table(self):
+    def get_query_create_table(self) -> str:
+        """Creates the query for "create table"."""
         query_create_table = f"""
         CREATE TABLE IF NOT EXISTS {self.table_name}
           ({', '.join(self.create_statements)});
@@ -31,6 +46,7 @@ class TableProperties:
         return query_create_table
 
     def get_query_insert_into(self):
+        """Creates the query for "insert into table"."""
 
         columns = self.columns
         data_types = self.data_types
